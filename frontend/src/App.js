22ab0c25cbf7bf58b8d14122c0c7273b8a4d2e7e
IG <iikim2511@gmail.com>
2022-09-08 22:32:35 -0400

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
 
 // import './App.css';
 
+/* 
+
+🦄🦄🦄 😎😎me - clone coding강의 들을때 당부
+
+강의들으면서 처음듣는 부분은 그 부분만 crash course 유튜브 강의 , google document search하면 됨
+👉 crash course 유튜브 강의 
+👉 google document search
+
+*/
+
 function App() {
   return (
     <div className="App">
